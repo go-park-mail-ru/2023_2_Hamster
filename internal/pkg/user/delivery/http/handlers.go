b82e87953cded1f636536510e681a5744307a059
Kosmatoff <dimka.komarov@bk.ru>
2023-10-02 12:16:11 +0300

    add request handler GetIdFromRequest

--- internal/pkg/user/delivery/http/handlers.go
+++ internal/pkg/user/delivery/http/handlers.go
@@ -2,7 +2,7 @@ package http
 
 import (
 	"net/http"
-
+	commonHttp "github.com/go-park-mail-ru/2023_2_Hamster/internal/common/http"
 	"github.com/go-park-mail-ru/2023_2_Hamster/internal/common/logger"
 	"github.com/go-park-mail-ru/2023_2_Hamster/internal/pkg/user"
 )
@@ -12,6 +12,10 @@ type Handler struct {
 	logger      logger.CustomLogger
 }
 
+const (
+	userIdUrlParam  = "userID"
+)
+
 func NewHandler(uu user.Usecase, l logger.CustomLogger) *Handler {
 	return &Handler{
 		userService: uu,
@@ -20,7 +24,13 @@ func NewHandler(uu user.Usecase, l logger.CustomLogger) *Handler {
 }
 
 func (h *Handler) GetBalance(w http.ResponseWriter, r *http.Request) {
-	commonHttp.
+	commonHttp.GetIDFromRequest(r ,userIdUrlParam)
+	if err != nil {
+		h.logger.infof("invalid id: %v:", err)
+		commonHttp.ErrorResponse(w, "invalid url parameter", http.StatusBadRequest, h.logger)
+		return
+	}
+
 }
 
 func (h* Handler) GetPlannedBudget(w http.ResponseWriter, r *http.Request) {
