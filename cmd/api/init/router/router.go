aae52f398faae6461e71c6a4294d1da7ec9774ab
linuxMate <grigorikovalenko@gmail.com>
2023-10-02 16:47:25 +0300

    serv

--- cmd/api/init/router/router.go
+++ cmd/api/init/router/router.go
@@ -1,16 +1,39 @@
 package router
 
 import (
+	"encoding/json"
+	"net/http"
+
 	auth "github.com/go-park-mail-ru/2023_2_Hamster/internal/pkg/auth/delivery/http"
 	"github.com/gorilla/mux"
 )
 
-func HelloHandler()
+type Response struct {
+	Status string `json:"status"`
+	Msg    string `json:"message"`
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Status: "200 OK",
+		Msg:    "Pong",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
 // Initialize router and describes all app's endpoints
 func InitRouter(auth *auth.Handler /*, user *user.Handler*/) *mux.Router {
 
 	r := mux.NewRouter()
+
+	r.Path("/ping").HandlerFunc(HelloHandler)
 
 	apiRouter := r.PathPrefix("/api").Subrouter()
 
