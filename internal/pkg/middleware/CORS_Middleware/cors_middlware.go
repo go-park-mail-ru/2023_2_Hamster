f5454bec1a7fd8b6e2144904dac9f7c56de4ee15
linuxMate <grigorikovalenko@gmail.com>
2023-10-05 23:53:05 +0300

    cors update

--- internal/pkg/middleware/CORS_Middleware/cors_middlware.go
+++ internal/pkg/middleware/CORS_Middleware/cors_middlware.go
@@ -19,8 +19,9 @@ const (
 )
 
 func CorsMiddleware(next http.Handler) http.Handler {
+	origin := os.Getenv("FRONTEND_IP") + ":" + os.Getenv("FRONTEND_PORT") + ", " + "127.0.0.1:8000"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set(Origin, os.Getenv("FRONTEND_IP")+os.Getenv("FRONTEND_PORT"))
+		w.Header().Set(Origin, origin)
 		w.Header().Set(Methods, AllowedMethods)
 		w.Header().Set(Headers, AllowedHeaders)
 		w.Header().Set(Credentials, "true")
