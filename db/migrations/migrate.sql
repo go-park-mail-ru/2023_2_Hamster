c5203cb255c99a77e67e25fa25e0a59b9d176940
linuxMate <grigorikovalenko@gmail.com>
2023-11-03 19:29:07 +0300

    varchar gone modern text

--- db/migrations/migrate.sql
+++ db/migrations/migrate.sql
@@ -2,22 +2,27 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS "user" (
     user_id        UUID         DEFAULT uuid_generate_v4() PRIMARY KEY,
-    username       VARCHAR(50)  UNIQUE                     NOT NULL,
-    full_name      VARCHAR(255)                            NOT NULL,
-    password_hash  VARCHAR(256)                            NOT NULL,
+    username       TEXT         UNIQUE                     NOT NULL,
+    full_name      TEXT                                    NOT NULL,
+    password_hash  TEXT                                    NOT NULL,
 	planned_budget MONEY        DEFAULT 0.00               NOT NULL,
-    avatar_url     UUID
+    avatar_url     UUID,
+    CHECK(LENGTH(usrname) <= 30),
+    CHECK(LENGTH(full_name) <= 60)
 );
 
 
 CREATE TABLE IF NOT EXISTS account (
-    account_id          UUID            DEFAULT uuid_generate_v4() PRIMARY KEY,
-    balance             MONEY           DEFAULT 0.00,
+    account_id          UUID         DEFAULT uuid_generate_v4() PRIMARY KEY,
+    balance             MONEY        DEFAULT 0.00,
     "description"       TEXT,
-    bank_name           VARCHAR(30),
-    currency            VARCHAR(3)   DEFAULT 'RUB',
+    bank_name           TEXT,
+    currency            TEXT         DEFAULT 'RUB',
     created_at          TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at          TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP NOT NULL
+    updated_at          TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CHECK(LENGTH("description") <= 255),
+    CHECK(LENGTH(bank_name) <= 60),
+    CHECK(LENGTH(currency) <= 60)
 );
 
 
@@ -31,7 +36,8 @@ CREATE TABLE IF NOT EXISTS user_account (
 CREATE TABLE IF NOT EXISTS category (
     category_id   UUID          DEFAULT uuid_generate_v4()   PRIMARY KEY,
     user_id       UUID          REFERENCES "user"(user_id)   CONSTRAINT fk_user_category  NOT NULL,
-    "name"        VARCHAR(30)                                                             NOT NULL
+    "name"        TEXT                                                                    NOT NULL,
+    CHECK(LENGTH("name") <= 60)
 );
 
 CREATE TABLE IF NOT EXISTS "transaction" (
@@ -42,10 +48,12 @@ CREATE TABLE IF NOT EXISTS "transaction" (
     is_income               BOOLEAN,
     total                   MONEY       DEFAULT 0.00,
     "date"                  DATE        DEFAULT CURRENT_DATE                NOT NULL,
-    payer_name              VARCHAR(40) DEFAULT ''                          NOT NULL,
+    payer_name              TEXT        DEFAULT ''                          NOT NULL,
     "description"           TEXT,
     created_at              TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP           NOT NULL,
-    updated_at              TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP           NOT NULL
+    updated_at              TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP           NOT NULL,
+    CHECK(LENGTH(payer_name) <= 50),
+    CHECK(LENGTH("description") <= 255)
 );
 
 
@@ -57,46 +65,52 @@ CREATE TABLE IF NOT EXISTS deposit (
     start_at      DATE          DEFAULT CURRENT_DATE            NOT NULL,
     end_at        DATE,
     created_at    TIMESTAMPTZ   DEFAULT CURRENT_TIMESTAMP       NOT NULL,
-    updated_at    TIMESTAMPTZ   DEFAULT CURRENT_TIMESTAMP       NOT NULL
+    updated_at    TIMESTAMPTZ   DEFAULT CURRENT_TIMESTAMP       NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS credit (
     credit_id           UUID        DEFAULT uuid_generate_v4()     PRIMARY KEY,
     account_id          UUID        REFERENCES account(account_id) NOT NULL,
     total_amount        MONEY       DEFAULT 0.00                   NOT NULL,
-    "status"            VARCHAR(20) DEFAULT 'active'               NOT NULL,
+    "status"            TEXT        DEFAULT 'active'               NOT NULL,
     monthly_payment     MONEY       DEFAULT 0.00,
     start_at            DATE        DEFAULT CURRENT_DATE,
+    calculation_details TEXT        DEFAULT '',
     end_at              DATE,
-    calculation_details VARCHAR(30) DEFAULT '',
     payments_received   MONEY       DEFAULT 0.00                   NOT NULL,
     created_at          TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP      NOT NULL,
-    updated_at          TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP      NOT NULL
+    updated_at          TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP      NOT NULL,
+    CHECK(LENGTH(calculation_details) <= 20),
+    CHECK(LENGTH("status") <= 60)
 );
 
 
 CREATE TABLE IF NOT EXISTS investment (
     investment_id   UUID         DEFAULT uuid_generate_v4() PRIMARY KEY,
     user_id         UUID         REFERENCES "user"(user_id) NOT NULL,
-    asset_type      VARCHAR(255)                            NOT NULL,
-    asset_name      VARCHAR(255)                            NOT NULL,
+    asset_type      TEXT                                    NOT NULL,
+    asset_name      TEXT                                    NOT NULL,
     purchase_price  MONEY        DEFAULT 0.00               NOT NULL,
     quantity        NUMERIC                                 NOT NULL,
     purchase_at     DATE         DEFAULT CURRENT_DATE       NOT NULL,
     created_at      TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP  NOT NULL,
-    updated_at      TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP  NOT NULL
+    updated_at      TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+    CHECK(LENGTH(asset_name) <= 70),
+    CHECK(LENGTH(asset_type) <= 40)
 );
 
 
 CREATE TABLE IF NOT EXISTS goal (
     goal_id       UUID            DEFAULT uuid_generate_v4() PRIMARY KEY,
     user_id       UUID            REFERENCES "user"(user_id) NOT NULL,
-    "name"        VARCHAR(255),
+    "name"        TEXT,
     "description" TEXT            DEFAULT '',
     "target"      MONEY           DEFAULT 0.00               NOT NULL,
     "date"        DATE,
     created_at    TIMESTAMPTZ     DEFAULT CURRENT_TIMESTAMP  NOT NULL,
-    updated_at    TIMESTAMPTZ     DEFAULT CURRENT_TIMESTAMP  NOT NULL
+    updated_at    TIMESTAMPTZ     DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+    CHECK(LENGTH("description") <= 255),
+    CHECK(LENGTH("name") <= 50)
 );
 
 CREATE OR REPLACE FUNCTION public.moddatetime()
